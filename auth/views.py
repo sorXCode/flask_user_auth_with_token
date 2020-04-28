6371e256975bda5726c7c8254bb81ad2ba9e7dfd
sorXCode <sorxcode@gmail.com>
2020-04-28 20:24:19 +0100

    added view

---
+++ auth/views.py
@@ -0,0 +1,83 @@
+import secrets
+from flask import Blueprint, Response, abort, g, jsonify
+from flask_httpauth import HTTPTokenAuth
+from flask_restful import Api, Resource, reqparse, request
+
+from .models import User
+
+auth_ = HTTPTokenAuth(scheme="Bearer")
+
+user_app = Blueprint('user_app', __name__)
+api = Api(user_app)
+
+
+class SignupAPI(Resource):
+    model = User
+    userParser = reqparse.RequestParser()
+    userParser.add_argument('email', type=str, nullable=False)
+    userParser.add_argument('password', type=str, nullable=False)
+
+    def post(self):
+        """
+        Creates new user
+        Parameters:
+            -email (str)
+            -password (str)
+        """
+        args = self.userParser.parse_args()
+        email = args['email']
+        password = args['password']
+        # TODO: Add validations to args
+        if not (email and password):
+            abort(400, "Some Parameter(s) are missing")
+        user = self.model.create_user(email=email,
+                                      password=password)
+        print(user, end='\n\n\n\n\n')
+        
+        if not user:
+            abort(409, "account found on platform, create_user unsuccessful.")
+        g.user = user
+        return {'status': 'success',
+                'message': 'Account Created, proceed to login',
+                }
+
+
+class LoginAPI(Resource):
+    model = User
+    userParser = reqparse.RequestParser()
+    userParser.add_argument('email', type=str, nullable=False)
+    userParser.add_argument('password', type=str, nullable=False)
+
+    def post(self):
+        args = self.userParser.parse_args()
+        response_object = {'status': 'Failed',
+                           'message': 'Incorrect email or password'}
+        email, password = args['email'], args['password']
+        user, auth_token = self.model.login(email, password)
+        if user:
+            response_object = jsonify({'status': 'Success',
+                               'message': 'Login successful',
+                               'auth_token': auth_token
+                               })
+        g.user = user
+        return response_object
+
+
+class Home(Resource):
+    @auth_.login_required
+    def get(self):
+        return {"msg": "Welcome to @Home_page",
+                "user": g.user.email}
+
+@auth_.verify_token
+def verify_token(token):
+    user = User.verify_auth_token(token)
+    if isinstance(user, str):
+        abort(400, {"error": user})
+    g.user = user
+    return True
+
+
+api.add_resource(SignupAPI, '/signup')
+api.add_resource(LoginAPI, '/login')
+api.add_resource(Home, '/')
