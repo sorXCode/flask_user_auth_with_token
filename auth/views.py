7b8e875fb84540f7b15b09dad2667e52c0eae3a8
sorXCode <sorxcode@gmail.com>
2020-04-29 13:23:38 +0100

    added views

--- auth/views.py
+++ auth/views.py
@@ -1,4 +1,6 @@
+import re
 import secrets
+
 from flask import Blueprint, Response, abort, g, jsonify
 from flask_httpauth import HTTPTokenAuth
 from flask_restful import Api, Resource, reqparse, request
@@ -27,13 +29,14 @@ class SignupAPI(Resource):
         args = self.userParser.parse_args()
         email = args['email']
         password = args['password']
-        # TODO: Add validations to args
+
         if not (email and password):
             abort(400, "Some Parameter(s) are missing")
+        if not validate_email(email):
+            abort(400, "Invalid Email supplied")
+
         user = self.model.create_user(email=email,
                                       password=password)
-        print(user, end='\n\n\n\n\n')
-        
         if not user:
             abort(409, "account found on platform, create_user unsuccessful.")
         g.user = user
@@ -52,13 +55,15 @@ class LoginAPI(Resource):
         args = self.userParser.parse_args()
         response_object = {'status': 'Failed',
                            'message': 'Incorrect email or password'}
+        
         email, password = args['email'], args['password']
         user, auth_token = self.model.login(email, password)
+        
         if user:
             response_object = jsonify({'status': 'Success',
-                               'message': 'Login successful',
-                               'auth_token': auth_token
-                               })
+                                       'message': 'Login successful',
+                                       'auth_token': auth_token
+                                       })
         g.user = user
         return response_object
 
@@ -69,14 +74,21 @@ class Home(Resource):
         return {"msg": "Welcome to @Home_page",
                 "user": g.user.email}
 
+
 @auth_.verify_token
 def verify_token(token):
     user = User.verify_auth_token(token)
+
     if isinstance(user, str):
         abort(400, {"error": user})
     g.user = user
     return True
 
+def validate_email(email):
+    email_regex = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+    if (re.search(email_regex,email)):
+        return True
+    return False
 
 api.add_resource(SignupAPI, '/signup')
 api.add_resource(LoginAPI, '/login')
--- config.py
+++ config.py
@@ -9,29 +9,24 @@ class Config:
         pass
 
 
-class DevelopmentConfig(Config):
+class Development(Config):
     SECRET_KEY = "HGTO6rmUxIC_4maze8TIj54BPJ7IRb--Awsz_1oSELs"
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
-class TestingConfig(Config):
+class Test(Config):
     # Add a secret key for testing
+    SECRET_KEY = "fca409881de74c0900c06066b946bd6dc324f193f0f9fbcd508316fd73db3d6b"
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite://'
+        'sqlite:///' + os.path.join(basedir, 'test_db.sqlite')
 
-
-class ProductionConfig(Config):
-    # Add a secret key for production
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
 config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig,
-    'default': DevelopmentConfig
+    'development': Development,
+    'test': Test,
+    'default': Development
 }
